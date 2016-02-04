b45ef305367baaf17f00efc7fae97f1ec5882c6d
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-02-04 19:47:47 +0000

    Updating the posts routes

--- src/Http/Routes/Foundation/PostsRoutes.php
+++ src/Http/Routes/Foundation/PostsRoutes.php
@@ -27,9 +27,62 @@ class PostsRoutes extends RouteRegister
             'as'        => 'posts.',
         ], function () {
             $this->get('/', [
-                'as'   => 'index', // blog::foundation.posts.index
+                'as'   => 'index',         // blog::foundation.posts.index
                 'uses' => 'PostsController@index',
             ]);
+
+            $this->get('trashed', [
+                'as'   => 'trash',         // blog::foundation.posts.trash
+                'uses' => 'PostsController@trash',
+            ]);
+
+            $this->get('create', [
+                'as'   => 'create',        // blog::foundation.posts.create
+                'uses' => 'PostsController@create',
+            ]);
+
+            $this->post('store', [
+                'as'   => 'store',         // blog::foundation.posts.store
+                'uses' => 'PostsController@store',
+            ]);
+
+            $this->group([
+                'prefix' => '{post_id}',
+            ], function () {
+                $this->get('show', [
+                    'as'   => 'show',      // blog::foundation.posts.show
+                    'uses' => 'PostsController@show',
+                ]);
+
+                $this->get('edit', [
+                    'as'   => 'edit',      // blog::foundation.posts.edit
+                    'uses' => 'PostsController@edit',
+                ]);
+
+                $this->put('update', [
+                    'as'   => 'update',    // blog::foundation.posts.update
+                    'uses' => 'PostsController@update',
+                ]);
+
+                $this->put('publish', [
+                    'as'   => 'publish',   // blog::foundation.posts.activate
+                    'uses' => 'PostsController@publish',
+                ]);
+
+                $this->put('restore', [
+                    'as'   => 'restore',   // blog::foundation.posts.restore
+                    'uses' => 'PostsController@restore',
+                ]);
+
+                $this->delete('delete', [
+                    'as'   => 'delete',    // blog::foundation.posts.delete
+                    'uses' => 'PostsController@delete',
+                ]);
+            });
         });
+
+        //$router->bind('post_id', function($postId) {
+        //    return Post::findOrFail($postId);
+        //});
     }
 }
