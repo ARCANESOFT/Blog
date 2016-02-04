31601efc65ed12a243a46a3598c8a954f3b69e0a
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-02-04 17:35:47 +0000

    Adding DatabaseSeeder

---
+++ src/Seeds/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php namespace Arcanesoft\Blog\Seeds;
+
+use Arcanesoft\Blog\Bases\Seeder;
+
+/**
+ * Class     DatabaseSeeder
+ *
+ * @package  Arcanesoft\Blog\Seeds
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class DatabaseSeeder extends Seeder
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Seeder collection.
+     *
+     * @var array
+     */
+    protected $seeds = [
+        //
+    ];
+}
