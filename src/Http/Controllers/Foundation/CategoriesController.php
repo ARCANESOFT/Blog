8fe723a1c1a9cbc798bbeac7950dd9db25504324
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-02-07 20:16:56 +0000

    Updating the controllers

--- src/Http/Controllers/Foundation/CategoriesController.php
+++ src/Http/Controllers/Foundation/CategoriesController.php
@@ -1,6 +1,10 @@
 <?php namespace Arcanesoft\Blog\Http\Controllers\Foundation;
 
 use Arcanesoft\Blog\Bases\FoundationController;
+use Arcanesoft\Blog\Http\Requests\Backend\Categories\CreateCategoryRequest;
+use Arcanesoft\Blog\Http\Requests\Backend\Categories\UpdateCategoryRequest;
+use Arcanesoft\Blog\Models\Category;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class     CategoriesController
@@ -11,15 +15,30 @@ use Arcanesoft\Blog\Bases\FoundationController;
 class CategoriesController extends FoundationController
 {
     /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The category model.
+     *
+     * @var \Arcanesoft\Blog\Models\Category
+     */
+    private $category;
+
+    /* ------------------------------------------------------------------------------------------------
      |  Constructor
      | ------------------------------------------------------------------------------------------------
      */
     /**
      * Instantiate the controller.
+     *
+     * @param  \Arcanesoft\Blog\Models\Category  $category
      */
-    public function __construct()
+    public function __construct(Category $category)
     {
         parent::__construct();
+
+        $this->category = $category;
 
         $this->setCurrentPage('blog-categories');
         $this->addBreadcrumbRoute('Categories', 'blog::foundation.categories.index');
@@ -29,14 +48,96 @@ class CategoriesController extends FoundationController
      |  Main Functions
      | ------------------------------------------------------------------------------------------------
      */
-    public function index()
+    public function index($trashed = false)
     {
         $this->authorize('blog.categories.list');
+
+        $categories = $this->category->with(['posts']);
+        $categories = $trashed
+            ? $categories->onlyTrashed()->paginate(30)
+            : $categories->paginate(30);
 
         $title = 'Blog - Categories';
         $this->setTitle($title);
         $this->addBreadcrumb('List all categories');
 
-        return $this->view('foundation.categories.list');
+        return $this->view('foundation.categories.list', compact('categories', 'trashed'));
+    }
+
+    public function trash()
+    {
+        return $this->index(true);
+    }
+
+    public function create()
+    {
+        $this->authorize('blog.categories.create');
+
+        $title = 'Blog - Categories';
+        $this->setTitle($title);
+        $this->addBreadcrumb('Create category');
+
+        return $this->view('foundation.categories.create');
+    }
+
+    public function store(CreateCategoryRequest $request, Category $category)
+    {
+        $this->authorize('blog.categories.create');
+
+        $category->fill($request->only(['name']));
+        $category->save();
+
+        $message = "The category {$category->name} was created successfully !";
+        Log::info($message, $category->toArray());
+        $this->notifySuccess($message, 'Category created !');
+
+        return redirect()->route('blog::foundation.categories.index');
+    }
+
+    public function show(Category $category)
+    {
+        $this->authorize('blog.categories.show');
+
+        $category->load(['posts']);
+
+        $title = 'Blog - Categories';
+        $this->setTitle($title);
+        $this->addBreadcrumb('Category - ' . $category->name);
+
+        return $this->view('foundation.categories.show', compact('category'));
+    }
+
+    public function edit(Category $category)
+    {
+        $this->authorize('blog.categories.update');
+
+        $title = 'Blog - Categories';
+        $this->setTitle($title);
+        $this->addBreadcrumb('Update category');
+
+        return $this->view('foundation.categories.edit', compact('category'));
+    }
+
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $this->authorize('blog.categories.update');
+
+        $category->update($request->only(['name']));
+
+        $message = "The category {$category->name} was updated successfully !";
+        Log::info($message, $category->toArray());
+        $this->notifySuccess($message, 'Category updated !');
+
+        return redirect()->route('blog::foundation.categories.show', [$category->id]);
+    }
+
+    public function restore(Category $category)
+    {
+        $this->authorize('blog.categories.update');
+    }
+
+    public function delete(Category $category)
+    {
+        $this->authorize('blog.categories.delete');
     }
 }
--- src/Http/Controllers/Foundation/TagsController.php
+++ src/Http/Controllers/Foundation/TagsController.php
@@ -1,6 +1,10 @@
 <?php namespace Arcanesoft\Blog\Http\Controllers\Foundation;
 
 use Arcanesoft\Blog\Bases\FoundationController;
+use Arcanesoft\Blog\Http\Requests\Backend\Tags\CreateTagRequest;
+use Arcanesoft\Blog\Http\Requests\Backend\Tags\UpdateTagRequest;
+use Arcanesoft\Blog\Models\Tag;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class     TagsController
@@ -11,15 +15,28 @@ use Arcanesoft\Blog\Bases\FoundationController;
 class TagsController extends FoundationController
 {
     /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * @var \Arcanesoft\Blog\Models\Tag
+     */
+    private $tag;
+
+    /* ------------------------------------------------------------------------------------------------
      |  Constructor
      | ------------------------------------------------------------------------------------------------
      */
     /**
      * Instantiate the controller.
+     *
+     * @param  \Arcanesoft\Blog\Models\Tag  $tag
      */
-    public function __construct()
+    public function __construct(Tag $tag)
     {
         parent::__construct();
+
+        $this->tag = $tag;
 
         $this->setCurrentPage('blog-tags');
         $this->addBreadcrumbRoute('Tags', 'blog::foundation.tags.index');
@@ -29,14 +46,95 @@ class TagsController extends FoundationController
      |  Main Functions
      | ------------------------------------------------------------------------------------------------
      */
-    public function index()
+    public function index($trashed = false)
     {
         $this->authorize('blog.tags.list');
+
+        $tags = $this->tag->with(['posts']);
+        $tags = $trashed
+            ? $tags->onlyTrashed()->paginate(30)
+            : $tags->paginate(30);
 
         $title = 'Blog - Tags';
         $this->setTitle($title);
         $this->addBreadcrumb('List all tags');
 
-        return $this->view('foundation.tags.list');
+        return $this->view('foundation.tags.list', compact('tags', 'trashed'));
+    }
+    public function trash()
+    {
+        return $this->index(true);
+    }
+
+    public function create()
+    {
+        $this->authorize('blog.tags.create');
+
+        $title = 'Blog - Tags';
+        $this->setTitle($title);
+        $this->addBreadcrumb('Create tag');
+
+        return $this->view('foundation.tags.create');
+    }
+
+    public function store(CreateTagRequest $request, Tag $tag)
+    {
+        $this->authorize('blog.tags.create');
+
+        $tag->fill($request->only(['name']));
+        $tag->save();
+
+        $message = "The tag {$tag->name} was created successfully !";
+        Log::info($message, $tag->toArray());
+        $this->notifySuccess($message, 'Tag created !');
+
+        return redirect()->route('blog::foundation.tags.index');
+    }
+
+    public function show(Tag $tag)
+    {
+        $this->authorize('blog.tags.show');
+
+        $tag->load(['posts']);
+
+        $title = 'Blog - Tags';
+        $this->setTitle($title);
+        $this->addBreadcrumb('Tag - ' . $tag->name);
+
+        return $this->view('foundation.tags.show', compact('tag'));
+    }
+
+    public function edit(Tag $tag)
+    {
+        $this->authorize('blog.tags.update');
+
+        $title = 'Blog - Tags';
+        $this->setTitle($title);
+        $this->addBreadcrumb('Update tag');
+
+        return $this->view('foundation.tags.edit', compact('tag'));
+    }
+
+    public function update(UpdateTagRequest $request, Tag $tag)
+    {
+        $this->authorize('blog.tags.update');
+
+        $tag->update($request->only(['name']));
+
+        $message = "The tag {$tag->name} was updated successfully !";
+        Log::info($message, $tag->toArray());
+        $this->notifySuccess($message, 'Tag updated !');
+
+        return redirect()->route('blog::foundation.tags.show', [$tag->id]);
+    }
+
+    public function restore(Tag $tag)
+    {
+        $this->authorize('blog.tags.update');
+    }
+
+    public function delete(Tag $tag)
+    {
+        $this->authorize('blog.tags.delete');
     }
 }
