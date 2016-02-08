39918ac49f09b16376ed4fa5d06eb509fdf5bca4
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-02-08 09:38:28 +0000

    Updating config file (Models, Tables & Observers)

--- config/blog.php
+++ config/blog.php
@@ -17,4 +17,26 @@ return [
     'route'    => [
         'prefix'    => 'blog',
     ],
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Models
+     | ------------------------------------------------------------------------------------------------
+     */
+    'posts' => [
+        'table'    => 'posts',
+        'model'    => Arcanesoft\Blog\Models\Post::class,
+        'observer' => Arcanesoft\Blog\Observers\PostObserver::class,
+    ],
+
+    'categories' => [
+        'table'    => 'categories',
+        'model'    => Arcanesoft\Blog\Models\Category::class,
+        'observer' => Arcanesoft\Blog\Observers\CategoryObserver::class,
+    ],
+
+    'tags' => [
+        'table'    => 'tags',
+        'model'    => Arcanesoft\Blog\Models\Tag::class,
+        'observer' => Arcanesoft\Blog\Observers\TagObserver::class,
+    ],
 ];
